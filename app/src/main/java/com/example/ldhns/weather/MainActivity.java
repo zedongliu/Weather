6da34ff7aa5bfa6cb775f2b8f099711de3980286
liuzedong <lzd_zedong@163.com>
2017-09-05 19:06:05 +0800

    解析网络请求到的XML数据

--- app/src/main/java/com/example/ldhns/weather/MainActivity.java
+++ app/src/main/java/com/example/ldhns/weather/MainActivity.java
@@ -8,9 +8,13 @@ import android.widget.Toast;
 import android.util.Log;
 import android.app.Activity;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -44,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void getWeatherDatafromNet(String cityCode){
-        final String address = "http://wthrcdn.cn/WeatherApi?citykey="+cityCode;
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey="+cityCode;
         Log.d("Address",address);
         new Thread(new Runnable() {
             @Override
@@ -67,10 +71,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                     String response = sb.toString();
                     Log.d("response",response);
+                    parseXML(response);
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             }
         }).start();
     }
+
+    private void parseXML(String xmlData){
+        int fengliCount = 0;
+        int fengxiangCount = 0;
+        int dateCount = 0;
+        int highCount = 0;
+        int lowCount = 0;
+        int typeCount = 0;
+        try{
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+
+            int eventType = xmlPullParser.getEventType();
+            Log.d("Weather","start parse xml");
+
+            while(eventType!=xmlPullParser.END_DOCUMENT){
+                switch (eventType){
+                    //文档开始位置
+                    case XmlPullParser.START_DOCUMENT:
+                        Log.d("parse","start doc");
+                        break;
+                    //标签元素开始位置
+                    case XmlPullParser.START_TAG:
+                        if(xmlPullParser.getName().equals("city")){
+                            eventType=xmlPullParser.next();
+                            Log.d("city",xmlPullParser.getText());
+                        }else if(xmlPullParser.getName().equals("updatetime")){
+                            eventType=xmlPullParser.next();
+                            Log.d("updatetime",xmlPullParser.getText());
+                        }else if(xmlPullParser.getName().equals("wendu")){
+                            eventType=xmlPullParser.next();
+                            Log.d("wendu",xmlPullParser.getText());
+                        }else if(xmlPullParser.getName().equals("fengli") && fengliCount == 0){
+                            eventType=xmlPullParser.next();
+                            Log.d("fengli",xmlPullParser.getText());
+                            fengliCount++;
+                        }else if(xmlPullParser.getName().equals("shidu")){
+                            eventType=xmlPullParser.next();
+                            Log.d("shidu",xmlPullParser.getText());
+                        }else if(xmlPullParser.getName().equals("fengxiang") && fengxiangCount == 0){
+                            eventType=xmlPullParser.next();
+                            Log.d("fengxiang",xmlPullParser.getText());
+                            fengxiangCount++;
+                        }else if(xmlPullParser.getName().equals("pm25")){
+                            eventType=xmlPullParser.next();
+                            Log.d("pm25",xmlPullParser.getText());
+                        }else if(xmlPullParser.getName().equals("quality"))
+                        {
+                            eventType=xmlPullParser.next();
+                            Log.d("quelity",xmlPullParser.getText());
+                        }else if(xmlPullParser.getName().equals("date") && dateCount==0 ) {
+                            eventType=xmlPullParser.next();
+                            Log.d("date",xmlPullParser.getText());
+                            dateCount++;
+                        }else if(xmlPullParser.getName().equals("high") && highCount==0 ) {
+                            eventType=xmlPullParser.next();
+                            Log.d("high",xmlPullParser.getText());
+                            highCount++;
+                        }else if(xmlPullParser.getName().equals("low") && lowCount==0 ){
+                            eventType=xmlPullParser.next();
+                            Log.d("low",xmlPullParser.getText());
+                            lowCount++;
+                        }else if(xmlPullParser.getName().equals("type") && typeCount==0 ) {
+                            eventType=xmlPullParser.next();
+                            Log.d("type",xmlPullParser.getText());
+                            typeCount++;
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        break;
+                }
+                eventType=xmlPullParser.next();
+            }
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 }
