2c3e6a66385fbadb7dcb79dcde605c8fc12ee283
liuzedong <lzd_zedong@163.com>
2017-09-06 11:04:22 +0800

    去掉标题栏，添加返回按钮

--- app/src/main/java/com/example/ldhns/weather/SelectCity.java
+++ app/src/main/java/com/example/ldhns/weather/SelectCity.java
@@ -2,15 +2,33 @@ package com.example.ldhns.weather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by ldhns on 2017/9/5.
  */
 
-public class SelectCity extends Activity {
+public class SelectCity extends Activity implements View.OnClickListener{
+    private ImageView backBtn;
+
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.select_city);
+
+        backBtn = (ImageView)findViewById(R.id.title_selectCity_back);
+        backBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.title_selectCity_back:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
